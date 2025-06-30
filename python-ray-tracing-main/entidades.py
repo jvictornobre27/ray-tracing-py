ed9426e26f9f87f353b1b2fc3042edf61fdd44a5
Bruno Carvalho <seu-email@exemplo.com>
2025-06-30 12:32:54 -0300

    Subindo mesh

--- python-ray-tracing-main/entidades.py
+++ python-ray-tracing-main/entidades.py
@@ -57,3 +57,66 @@ class Plane: #representa um plano 3D
         t = sum(n * dp for n, dp in zip(self.normal, d)) / denominator #calcula o "quanto andar" (parâmetro t) para alcançar o plano ao longo do vetor da linha
 
         return tuple(lp + t * lv for lp, lv in zip(line_point, line_vector)) #achar as coordenadas exatas do ponto de interseção
+
+#--------------------------------SEGUNDA ENTREGA ADICIONAIS--------------------------------------------------------------------------
+
+class Mesh: #representa uma malha
+
+    def __init__(
+        self,
+        triangle_quantity: int,
+        vertices_quantity: int,
+        vertices: list[Ponto],
+        triangle_tuple_vertices: list[tuple[int, int, int]],
+        triangle_normals: list,
+        vertex_normals: list,
+        color,
+    ):
+        self.triangle_quantity = triangle_quantity
+        self.vertices_quantity = vertices_quantity
+        self.vertices = vertices
+        self.triangle_tuple_vertices = triangle_tuple_vertices #lista de tuplas com índices de vértices que formam os triângulos
+        self.triangle_normals = triangle_normals #uma normal (vetor perpendicular) para cada triângulo
+        self.vertex_normals = vertex_normals
+
+        self.color = color
+
+    def __point_in_triangle__(self, point, triangle_vertices): #Verifica se um ponto esta dentro de um triangulo usando coord baricentrica
+
+        #Cria vetores entre os vértices do triângulo.
+        v0 = triangle_vertices[2].__sub__(triangle_vertices[0]) 
+        v1 = triangle_vertices[1].__sub__(triangle_vertices[0])
+        v2 = point.__sub__(triangle_vertices[0])
+
+        d00 = v0.__mul__(v0)
+        d01 = v0.__mul__(v1)
+        d11 = v1.__mul__(v1)
+        d20 = v2.__mul__(v0)
+        d21 = v2.__mul__(v1)
+
+        denom = d00 * d11 - d01 * d01
+
+        v = (d11 * d20 - d01 * d21) / denom
+        w = (d00 * d21 - d01 * d20) / denom
+        u = 1.0 - v - w
+
+        return (v >= 0) and (w >= 0) and (u >= 0)
+
+    def __intersect_line__(self, line_point, line_vector): #Calcula a interseção de o ponto de interseção entre a malha e uma linha
+        for index, triangle in enumerate(self.triangle_tuple_vertices):
+            triangle_vertices = [self.vertices[i] for i in triangle]
+            triangle_normal = self.triangle_normals[index]
+            plane = Plane(triangle_vertices[0], triangle_normal, self.color)
+            intersection_point = plane.__intersect_line__(line_point, line_vector)
+            if intersection_point is not None:
+                intersection_point = Ponto(
+                    intersection_point[0], intersection_point[1], intersection_point[2]
+                )
+                if self.__point_in_triangle__(intersection_point, triangle_vertices):
+                    return (
+                        intersection_point.x,
+                        intersection_point.y,
+                        intersection_point.z,
+                    )
+        return None
+
